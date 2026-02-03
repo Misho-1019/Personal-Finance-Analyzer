2afcc0f63153537436532654a1b951e54ec32455
Misho1019 <mikailtorres99@gmail.com>
2026-02-03 04:55:41 +0200

    Create auth controller

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import router from "./router.js";
 
 dotenv.config();
 
 const app = express()
 
-const port = process.env.PORT || 3030;
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(router)
+
+const port = process.env.PORT || 3030;
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
