bca3a818a87110c9100f2c52f1ef561ce03775aa
Misho1019 <mikailtorres99@gmail.com>
2026-02-03 04:51:24 +0200

    Install and set up express

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,11 @@
-console.log('It works!');
+import express from "express";
+
+const app = express()
+
+const port = process.env.PORT || 3030;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
