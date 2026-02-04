878c0ec16635e2b24017956857691884aa758915
Misho1019 <mikailtorres99@gmail.com>
2026-02-04 18:51:59 +0200

    Hash password on register

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -1,15 +1,18 @@
 import prisma from "../prismaClient.js";
+import bcrypt from "bcrypt";
 
 export default {
     async register(authData) {
         const {firstName, lastName, email, password} = authData;
+
+        const passwordHash = await bcrypt.hash(password, 10)
 
         const user = await prisma.user.create({
             data: {
                 firstName,
                 lastName,
                 email,
-                password
+                passwordHash
             }
         })
 
