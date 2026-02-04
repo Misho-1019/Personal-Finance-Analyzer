d285be350ee9f1cf58b85a0b36b79bd413336804
Misho1019 <mikailtorres99@gmail.com>
2026-02-04 18:38:52 +0200

    Create Prisma client

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import authService from "../services/authService";
+import authService from "../services/authService.js";
 
 const authController = Router();
 
 authController.post('/register', async (req, res) => {
     const authData = req.body;
 
-    await authService.register(authData)
+    const user = await authService.register(authData)
     
-    res.end();
+    return res.status(201).json(user)
 })
 
 export default authController;
