7591f9293f7b9f54277e50b4d724b0f9bb9337a1
Misho1019 <mikailtorres99@gmail.com>
2026-02-04 20:01:00 +0200

    Create login functionality with jwt generation

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -11,4 +11,17 @@ authController.post('/register', async (req, res) => {
     return res.status(201).json(user)
 })
 
+authController.post('/login', async (req, res) => {
+    const authData = req.body;
+
+    try {
+        const { token, payload } = await authService.login(authData);
+
+        return res.status(200).json(payload)
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json(error.message)
+    }
+})
+
 export default authController;
--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -1,5 +1,6 @@
 import prisma from "../prismaClient.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default {
     async register(authData) {
@@ -17,5 +18,37 @@ export default {
         })
 
         return user;
+    },
+    async login(authData) {
+        const { email, password } = authData;
+
+        const user = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        if (!user) {
+            throw new Error('Invalid email or password')
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.passwordHash)
+
+        if (!isPasswordValid) {
+            throw new Error('Invalid email or password')
+        }
+
+        const payload = {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            password: user.passwordHash
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '2h' })
+
+        return {
+            token,
+            payload,
+        }
     }
 }
