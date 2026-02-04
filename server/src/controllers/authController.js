d2259a98d709b0efdcea975f4b42ab69d9b90b6d
Misho1019 <mikailtorres99@gmail.com>
2026-02-04 02:11:43 +0200

    Add route definition for register

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -2,4 +2,12 @@ import { Router } from "express";
 
 const authController = Router();
 
+authController.post('/register', (req, res) => {
+    const authData = req.body;
+
+    console.log(authData);
+    
+    res.end();
+})
+
 export default authController;
