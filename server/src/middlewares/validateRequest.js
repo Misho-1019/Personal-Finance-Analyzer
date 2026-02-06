bf13352d49b2a0ef466ca55b5b58f10aad273f92
Misho1019 <mikailtorres99@gmail.com>
2026-02-06 03:05:35 +0200

    Create validateRequest middleware

---
+++ server/src/middlewares/validateRequest.js
@@ -0,0 +1,59 @@
+export function validateRequest({ body, query, params } = {}) {
+    return (req, res, next) => {
+        const errors = [];
+
+        if (body) {
+            const result = body.safeParse(req.body)
+
+            if (!result.success) {
+                errors.push(
+                    ...result.error.errors.map((e) => ({
+                        location: "body",
+                        field: e.path.join('.'),
+                        message: e.message,
+                    }))
+                )
+            } else {
+                req.body = result.data
+            }
+        }
+
+        if (query) {
+            const result = query.safeParse(req.query)
+
+            if (!result.success) {
+                errors.push(
+                    ...result.error.errors.map((e) => ({
+                        location: 'query',
+                        field: e.path.join('.'),
+                        message: e.message,
+                    }))
+                )
+            } else {
+                req.query = result.data
+            }
+        }
+
+        if (params) {
+            const result = params.safeParse(req.params)
+
+            if (!result.success) {
+                errors.push(
+                    ...result.error.errors.map((e) => ({
+                        location: 'params',
+                        field: e.path.join('.'),
+                        message: e.message,
+                    }))
+                )
+            } else {
+                req.params = result.data
+            }
+        }
+
+        if (errors.length > 0) {
+            return res.status(400).json({ errors })
+        }
+
+        next()
+    }
+}
