c77b489e363437b2e7adeba1442ada4d3dca42c1
Misho1019 <mikailtorres99@gmail.com>
2026-02-10 03:26:45 +0200

    Create category update endpoint

--- server/src/controllers/categoryController.js
+++ server/src/controllers/categoryController.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { isAuth } from "../middlewares/authMiddleware.js";
 import { validateRequest } from "../middlewares/validateRequest.js";
-import { createCategorySchema } from "../validators/categoriesSchema.js";
+import { createCategorySchema, updateCategorySchema } from "../validators/categoriesSchema.js";
 import categoryService from "../services/categoryService.js";
+import { idParamsSchema } from "../validators/transactionSchema.js";
 
 const categoryController = Router();
 
@@ -31,6 +32,24 @@ categoryController.get('/', isAuth, async (req, res) => {
         
         return res.status(200).json(categories)
     } catch (error) {
+        return res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+categoryController.patch('/:id', isAuth, validateRequest({ params: idParamsSchema, body: updateCategorySchema }), async (req, res) => {
+    const userId = req.user.id;
+    const categoryId = req.validated.params.id;
+    const updateData = req.validated.body;
+
+    try {
+        const updatedCategory = await categoryService.update(userId, categoryId, updateData);
+
+        return res.status(200).json(updatedCategory)
+    } catch (error) {
+        if (error.status) {
+            return res.status(error.status).json({ error: error.message })
+        }
+
         return res.status(500).json({ error: 'Internal server error' })
     }
 })
