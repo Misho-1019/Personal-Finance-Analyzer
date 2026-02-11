86dcb7eec8541c445270cf8282b19deb9c9ad3d4
Misho1019 <mikailtorres99@gmail.com>
2026-02-11 02:30:12 +0200

    Implement create service

---
+++ server/src/services/categoryKeywordService.js
@@ -0,0 +1,52 @@
+import prisma from "../prismaClient.js";
+
+export default {
+    async create(userId, { keyword, categoryId }) {
+        const normalizedKeyword = keyword.toLowerCase().trim().replace(/\s+/g, ' ');
+
+        const findCategory = await prisma.category.findFirst({
+            where: {
+                id: categoryId,
+                userId,
+            }
+        })
+
+        if (!findCategory) {
+            const err = new Error("Category not found");
+            err.status = 404;
+            throw err;
+        }
+
+        const existingKeyword = await prisma.categoryKeyword.findFirst({
+            where: {
+                userId,
+                keyword: { equals: normalizedKeyword },
+            }
+        })
+
+        if (existingKeyword) {
+            const err = new Error("Keyword already exists");
+            err.status = 409;
+            throw err;
+        }
+
+        const categoryKeyword = await prisma.categoryKeyword.create({
+            data: {
+                userId,
+                categoryId,
+                keyword: normalizedKeyword,
+            },
+            include: {
+                category: {
+                    select: {
+                        id: true,
+                        name: true,
+                        color: true,
+                    }
+                }
+            }
+        })
+
+        return categoryKeyword;
+    }
+}
