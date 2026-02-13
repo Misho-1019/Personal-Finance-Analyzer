13646169255f4f2270a043ef8cfe583a0b1924d5
Misho1019 <mikailtorres99@gmail.com>
2026-02-13 04:49:22 +0200

    Create categories summary endpoint

--- server/src/controllers/analyticsController.js
+++ server/src/controllers/analyticsController.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { isAuth } from "../middlewares/authMiddleware.js";
 import { validateRequest } from "../middlewares/validateRequest.js";
-import { analyticsSchema } from "../validators/analytcisSchema.js";
+import { analyticsSchema, categoriesAnalyticsSchema } from "../validators/analytcisSchema.js";
 import analyticsService from "../services/analyticsService.js";
 
 const analyticsController = Router();
@@ -12,6 +12,19 @@ analyticsController.get('/monthly-summary', isAuth, validateRequest({ query: ana
 
     try {
         const summary = await analyticsService.monthlySummary(userId, { from, to })
+
+        return res.status(200).json(summary)
+    } catch (error) {
+        return res.status(500).json({ error: 'An error occurred while fetching analytics data' })
+    }
+})
+
+analyticsController.get('/categories-summary', isAuth, validateRequest({ query: categoriesAnalyticsSchema }), async (req, res) => {
+    const userId = req.user.id;
+    const { from, to, type } = req.validated.query;
+
+    try {
+        const summary = await analyticsService.categoriesSummary(userId, { from, to, type })
 
         return res.status(200).json(summary)
     } catch (error) {
