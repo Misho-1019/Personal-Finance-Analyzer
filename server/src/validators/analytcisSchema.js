0891d357d1ec57725c5f3d128d265731948d2ea6
Misho1019 <mikailtorres99@gmail.com>
2026-02-13 03:06:34 +0200

    Create analytics schema

---
+++ server/src/validators/analytcisSchema.js
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const analyticsSchema = z.object({
+    from: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).refine((date) => {
+        const parsedDate = Date.parse(date);
+        return !isNaN(parsedDate);
+    }).optional(),
+    to: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).refine((date) => {
+        const parsedDate = Date.parse(date);
+        return !isNaN(parsedDate);
+    }).optional(),
+}).strict().superRefine(({ from, to }, ctx) => {
+    if (from && to) {
+        const fromDate = Date.parse(from);
+        const toDate = Date.parse(to);
+    
+        if (fromDate > toDate) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: "From date must be before or equal to To date",
+            });
+        }
+    }
+});
