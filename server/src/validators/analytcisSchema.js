889e7b74d4c2a39fb2cf3293edcea576ec416927
Misho1019 <mikailtorres99@gmail.com>
2026-02-13 04:49:02 +0200

    Add categories analytics validation schema

--- server/src/validators/analytcisSchema.js
+++ server/src/validators/analytcisSchema.js
@@ -22,3 +22,25 @@ export const analyticsSchema = z.object({
         }
     }
 });
+
+export const categoriesAnalyticsSchema = z.object({
+    from: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).refine((date) => {
+        const parsedDate = Date.parse(date);
+        return !isNaN(parsedDate);
+    }),
+    to: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).refine((date) => {
+        const parsedDate = Date.parse(date);
+        return !isNaN(parsedDate);
+    }),
+    type: z.enum(["INCOME", "EXPENSE"]).optional(),
+}).strict().superRefine(({ from, to }, ctx) => {
+    const fromDate = Date.parse(from);
+    const toDate = Date.parse(to);
+
+    if (fromDate > toDate) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "From date must be before or equal to To date",
+        });
+    }
+});
