d77993a1a876ec549484e65bcdefbd4347ce79b7
Misho1019 <mikailtorres99@gmail.com>
2026-02-15 22:36:53 +0200

    Create abort controller

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,19 +1,26 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import request from "../utils/requester.js";
 
 const baseUrl = 'http://localhost:3030/auth';
 
 export const useLogin = () => {
     const [isPending, setIsPending] = useState(false)
+    const abortRef = useRef(new AbortController())
 
     const login = async (email, password) => {
         setIsPending(true)
 
-        const result = await request.post(`${baseUrl}/login`, { email, password })
+        const result = await request.post(`${baseUrl}/login`, { email, password }, { signal: abortRef.current.signal })
         setIsPending(false)
 
         return result;
     }
+
+    useEffect(() => {
+        const abortController = abortRef.current;
+
+        return () => abortController.abort();
+    }, [])
 
     return { login, isPending }
 }
