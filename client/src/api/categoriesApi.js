1a58c67a83c3b7131efce2c95ca5adb87da31259
Misho1019 <mikailtorres99@gmail.com>
2026-02-23 03:48:49 +0200

    Refactor categoriesService

--- client/src/api/categoriesApi.js
+++ client/src/api/categoriesApi.js
@@ -3,25 +3,6 @@ import request from "../utils/requester.js";
 
 const baseUrl = 'http://localhost:3030/categories';
 
-export default {
-    async getCategories() {
-        const result = await request.get(baseUrl)
-
-        const categories = Object.values(result)
-
-        return categories;
-    },
-    async createCategory(categoryData) {
-        return await request.post(baseUrl, categoryData)
-    },
-    async updateCategory(categoryId, categoryData) {
-        return await request.patch(`${baseUrl}/${categoryId}`, categoryData)
-    },
-    async deleteCategory(categoryId) {
-        return await request.delete(`${baseUrl}/${categoryId}`)
-    }
-}
-
 export const useGetCategories = () => {
     const [isCategoriesLoading, setIsCategoryLoading] = useState(true)
     const [categories, setCategories] = useState([])
